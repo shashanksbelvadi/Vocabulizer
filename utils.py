4cf5b3ef98309a07d644e74a472cea5c84a445d1
Shashank Belvadi <shashanksbelvadi@gmail.com>
2020-04-09 00:16:00 -0700

    Updating language code.

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import requests
 
 
 def get_response(word_id):
-    url = 'https://od-api.oxforddictionaries.com/api/v1/entries/' + 'en' + '/' + word_id.lower()
+    url = 'https://od-api.oxforddictionaries.com/api/v2/entries/en-us/' + word_id.lower()
     response = requests.get(url, headers={'app_id': config('app_id'), 'app_key': config('app_key')})
 
     return response
