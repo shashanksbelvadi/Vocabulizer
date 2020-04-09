906b50b5c539e9a49d4b4106844fa6182875dcc0
Shashank Belvadi <shashanksbelvadi@gmail.com>
2020-04-08 22:55:11 -0700

    Updating .gitignore

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ import requests
 
 def get_response(word_id):
     url = 'https://od-api.oxforddictionaries.com/api/v1/entries/' + 'en' + '/' + word_id.lower()
-    response = requests.get(url, headers={'app_id': '01f441e0', 'app_key': 'cc85bf3d958d316f86140d34370369ec'})
+    response = requests.get(url, headers={'app_id': config('app_id'), 'app_key': config('app_key')})
 
     return response
