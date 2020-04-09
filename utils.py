335f585d304adb9d974649be4cabcb0f630de82c
Shashank Belvadi <shashanksbelvadi@gmail.com>
2020-04-08 23:05:26 -0700

    Removing print statements.

--- utils.py
+++ utils.py
@@ -5,7 +5,5 @@ import requests
 def get_response(word_id):
     url = 'https://od-api.oxforddictionaries.com/api/v1/entries/' + 'en' + '/' + word_id.lower()
     response = requests.get(url, headers={'app_id': config('app_id'), 'app_key': config('app_key')})
-    print(config('app_id'))
-    print(config('app_key'))
 
     return response
